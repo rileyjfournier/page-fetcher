d236fcb0bf7f7714733062a822a8edb6c55c6973
Riley Fournier <rileyjfournier@gmail.com>
2021-02-11 01:25:17 +0000

    added file size reading

--- fetcher.js
+++ fetcher.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 const args = process.argv.slice(2);
 
 request(args[0], (error, response, body) => {
-  // console.log('error:', error);
-  // console.log('statusCode:', response && response.statusCode);
-  // console.log('body:', body);
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', body);
+  const fileSize = Buffer.byteLength(JSON.stringify(body));
   fs.writeFile(args[1], body, function(err) {
     if (err) throw err;
+    console.log(`Downloaded and saved ${fileSize} bytes to ${args[1]}`)
   })
 });
